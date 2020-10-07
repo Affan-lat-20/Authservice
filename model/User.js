fecea2be462a6189248fe10baaa1084d40216b8b
Affan-lat-20 <72376364+Affan-lat-20@users.noreply.github.com>
2020-10-07 17:49:31 +0500

    updated user.js
    7th oct

--- model/User.js
+++ model/User.js
@@ -2,127 +2,96 @@ const mongoose = require('mongoose');
 // const Branduser = require('../model/Branduser');
 var Schema = mongoose.Schema;
 
+const userDetails= new Schema({
+    id :mongoose.Schema.Types.ObjectId,
 
-const branduserSchema = new Schema(
-    {
-        // Companyid:{  type: mongoose.Schema.Types.ObjectId,
-        //     ref: 'User' },
-        firstName:{
-            type: String,
-            
-            min:4,
-            max:255
+    firstName:{
+        type: String,
+        required:true,
+        min:4,
+        max:20
 
 
-            },
-        lastName:{
-                type: String,
-                
-                min:4,
-                max:255   
-    
-                },
-                
-         email:{
-                type: String,
-                
-                min:6,
-                max:255
-                 },
-         phonenumber:{
-                type : String,
-                
-                max:30
-            },
-
-         companyName:{
-                type: String,
-                
-                min:5,
-                max:255
-                
-             },
-         role:{
-                type: String,
-                
-                min:5,
-                max:255
-                
-             }
-
-        
-
-
- });
-
-const userSchema = new Schema(
-    {
-        firstName:{
+        },
+    lastName:{
             type: String,
             required:true,
             min:4,
-            max:20
-
+            max:20  
 
             },
-        lastName:{
+    country:{
                 type: String,
                 required:true,
                 min:4,
-                max:20  
+                max:20
+    
     
                 },
-        country:{
-                    type: String,
-                    required:true,
-                    min:4,
-                    max:20
+    city:{
+              type: String,
+              required:true,
+              min:5,
+              max:20
         
         
-                    },
-        city:{
-                  type: String,
-                  required:true,
-                  min:5,
-                  max:20
-            
-            
-                    },
-        companyName:{
-                type: String,
-                required:true,
-                min:5,
-                max:25
-                
-                
-                        },
-        websiteUrl:{
-                type: String,
-                required:true,
-                min:5,
-                max:25
-                    
-                    
-                         },
-        email:{
-                type: String,
-                required: true,
-                min:6,
-                max:100
-            },
+                }
 
-        password:{
+    ,
+     email:{
+             type: String,
+             required: true,
+             min:6,
+             max:100
+    },
+
+     password:{
             type: String,
             required: true,
             min:6,
             max:255
+            }
+
+})
+
+
+const userSchema = new Schema(
+    {
+        userType: {
+            type: String,
+            enum : ['brand','influencer'],
+            // default: 'user'
+                  },
+        eConcent: {
+                 isAccpeted:{type: Boolean},
+                 date:{
+                       type: Date,
+                      default: Date.now
+                       },
+                 signature:{type: String}
+                 },
+
+       
+
+        brand:{
+            brandDetails:userDetails,
+            // brandDetails:{  type: mongoose.Schema.Types.ObjectId,
+            //     ref: 'userDetails' },
+            brandCompany:{
+                companyName:{type: String},
+		        corporateHierarchy:{type: String},
+		        websiteUrl:{type: String},
+		        addressUrl:{type: String}
+
+            }
+
         },
-        econcent: {
-             type: Boolean,
-             required: true,
-             default: 1
-            },
-        brandusers: [branduserSchema] 
+
+        influencer:{
+            influencerDetails:userDetails
+
+        }
+
         ,
         date:{
             type: Date,
@@ -140,7 +109,7 @@ const userSchema = new Schema(
 
 
 // Custom validation for email
-userSchema.path('email').validate((val) => {
+userDetails.path('email').validate((val) => {
     emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(val);
 }, 'Invalid e-mail.');
